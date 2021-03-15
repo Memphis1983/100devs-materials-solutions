661fcf958134d2ec2598db438700a9af3a15d0c2
Memphis83 <kiran.iyer83@gmail.com>
2021-03-15 22:01:34 +0530

    super review materials

---
+++ 100devs-officehours-review-class 03-14-2021/super-review/simple-functions/js/main.js
@@ -0,0 +1,12 @@
+//---Easy
+//create a function that subtracts two numbers and alerts the difference
+
+//create a function that divides three numbers and console logs the quotient
+
+//create a function that multiplys three numbers and returns the product
+
+//---Medium
+//create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+
+//---Hard
+//create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
---
+++ 100devs-officehours-review-class 03-14-2021/super-review/variable-fun-time/js/main.js
@@ -0,0 +1,18 @@
+//--- Easy
+//create a variable and assign it a number
+
+//minus 10 from that number
+
+//print that number to the console
+
+//--- Medium
+//create a variable that holds a value from the input
+
+//add 25 to that number
+
+//alert that number
+
+//--- Hard
+//create a variable that holds the h1
+
+//add an event listener to that element that console logs the sum of the two previous variables
