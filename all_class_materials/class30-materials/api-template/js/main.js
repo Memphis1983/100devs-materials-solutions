98cbf848a4521814dedd2ea5870371ba46db7ce1
Memphis83 <kiran.iyer83@gmail.com>
2021-03-18 16:08:45 +0530

    updating all class materials and solutions

---
+++ all_class_materials/class30-materials/api-template/js/main.js
@@ -0,0 +1,1 @@
+//Make an api request using async await
---
+++ all_class_materials/class30-materials/node-backend-simple-json/js/main.js
@@ -0,0 +1,13 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personOccupation").textContent = data.currentOccupation
+}
