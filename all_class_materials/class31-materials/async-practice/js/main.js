291ed29f8e5828f0b6afb973e1aea177df0e24f2
Memphis83 <kiran.iyer83@gmail.com>
2021-03-20 21:08:03 +0530

    class materials update

--- all_class_materials/class31-materials/async-practice/js/main.js
+++ all_class_materials/class31-materials/async-practice/js/main.js
@@ -1,48 +1,48 @@
-// Code 01
-// function houseOne() {
-//   console.log("Paper delivered to house 1");
+//Code 01
+// function houseOne(){
+//     console.log('Paper delivered to house 1')
 // }
-// function houseTwo() {
-//   console.log("Paper delivered to house 2");
+// function houseTwo(){
+//     console.log('Paper delivered to house 2')
 // }
-// function houseThree() {
-//   console.log("Paper delivered to house 3");
+// function houseThree(){
+//     console.log('Paper delivered to house 3')
 // }
-// houseOne();
-// houseTwo();
-// houseThree();
+// houseOne()
+// houseTwo()
+// houseThree()
 
 //Code 02
-// function houseOne() {
-//   console.log("Paper delivered to house 1");
+// function houseOne(){
+//     console.log('Paper delivered to house 1')
 // }
-// function houseTwo() {
-//   setTimeout(() => console.log("Paper delivered to house 2"), 3000);
+// function houseTwo(){
+//     setTimeout(() => console.log('Paper delivered to house 2'), 3000)
 // }
-// function houseThree() {
-//   console.log("Paper delivered to house 3");
+// function houseThree(){
+//     console.log('Paper delivered to house 3')
 // }
-// houseOne();
-// houseTwo();
-// houseThree();
+// houseOne()
+// houseTwo()
+// houseThree()
 
-//* Code 03
-// function houseOne() {
-//   console.log("Paper delivered to house 1");
+//Code 03
+// function houseOne(){
+//     console.log('Paper delivered to house 1')
 // }
-// function houseTwo(callback) {
-//   setTimeout(() => {
-//     console.log("Paper delivered to house 2");
-//     callback();
-//   }, 3000);
+// function houseTwo(callback){
+//     setTimeout(() => {
+//         console.log('Paper delivered to house 2')
+//         callback()
+//     }, 3000)
 // }
-// function houseThree() {
-//   console.log("Paper delivered to house 3");
+// function houseThree(){
+//     console.log('Paper delivered to house 3')
 // }
-// houseOne();
-// houseTwo(houseThree);
+// houseOne()
+// houseTwo(houseThree)
 
-// Code 04
+//Code 04
 // function houseOne(){
 //     setTimeout(() => {
 //         console.log('Paper delivered to house 1')
@@ -56,11 +56,11 @@
 // }
 // houseOne()
 
-// Code 05
+//Code 05
 // const promise = new Promise((resolve, reject) => {
 //     const error = false
 //     if(!error){
-//         resolve('Promise has been fulfilled')
+//         resolve('Promise has been fullfilled')
 //     }else{
 //         reject('Error: Operation has failed')
 //     }
@@ -69,8 +69,6 @@
 // promise
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
-
-
 
 //Code 06
 // function houseOne(){
@@ -124,6 +122,7 @@
 //         }, 2000)
 //     })
 // }
+
 // async function getPaid(){
 //     const houseOneWait = await houseOne()
 //     const houseTwoWait = await houseTwo()
@@ -132,20 +131,14 @@
 //     console.log(houseTwoWait)
 //     console.log(houseThreeWait)
 // }
-//
+
 // getPaid()
 
 //Code 08
-try {
-    async function getACuteDogPhoto(){
-        const res = await fetch('https://dog.ceo/api/breeds/image/random')
-        const data = await res.json()
-        console.log(data)
-    }
-} catch (e) {
-    console.log(`Error: ${e}`)
-} finally {
-    
-}
+// async function getACuteDogPhoto(){
+//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
+//     const data = await res.json()
+//     console.log(data)
+// }
+// getACuteDogPhoto()
 
-getACuteDogPhoto()
---
+++ all_class_materials/class31-materials/node-intro-demo/demo.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const fs = require('fs')
+http.createServer((req, res) => {
+  fs.readFile('demofile.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+}).listen(8000)
